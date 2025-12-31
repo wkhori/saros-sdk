5e90222fb4da1c0890173bdcef648b13cc0fd91f
Walid Khori <34014558+wkhori@users.noreply.github.com>
2025-12-31 13:41:46 -0600

    remove build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: false,
+  dts: {
+    resolve: true,
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
